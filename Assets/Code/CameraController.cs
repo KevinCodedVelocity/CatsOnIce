5a6db237741f20bcfc88951d743cb3b4b523e42d
Kevin <kevinh@codedvelocity.com>
2018-09-01 18:38:52 -0500

    Cats on Ice - initial commit with Unity2018 project

---
+++ Assets/Code/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject target;
+    public float lerpFactor;
+    public float lerpMinimumDistance;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetPosition = target.transform.position;
+        Vector3 cameraPosition = gameObject.transform.position;
+        
+        gameObject.transform.LookAt(targetPosition);
+
+        // Find an offset from the target to move towards
+        Vector3 lerpTarget = targetPosition;
+        lerpTarget.y = cameraPosition.y;
+        Vector3 targetToCameraVector = lerpTarget - cameraPosition;
+        Vector3 offset = targetToCameraVector.normalized * lerpMinimumDistance;
+        lerpTarget += offset;
+
+        gameObject.transform.position = Vector3.Lerp(
+                cameraPosition,
+                lerpTarget,
+                lerpFactor);
+	}
+}
---
+++ Assets/Code/CatController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour {
+
+    public float torque;
+    public float force = 1000;
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.transform.position = new Vector3(0,0,0);
+        rigidBody = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            var forceVector = gameObject.transform.forward * force;
+            rigidBody.AddForce(forceVector);
+        }
+        
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            var forceVector = gameObject.transform.forward * -force;
+            rigidBody.AddForce(forceVector);
+        }
+        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidBody.AddTorque(new Vector3(0, -torque, 0));
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidBody.AddTorque(new Vector3(0, torque, 0));
+        }
+
+
+        //var rotation = gameObject.transform.eulerAngles;
+        //rotation.y += Time.deltaTime * 10;
+        //gameObject.transform.eulerAngles = rotation;
+
+
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name.ToLower() == "box")
+        {
+            rigidBody.AddForce(0, 100000, 0);
+        }
+    }
+
+    public void OnTriggerEnter(Collider otherCollider)
+    {
+        if (otherCollider.gameObject.name.ToLower() == "finishline")
+        {
+            rigidBody.AddForce(0, 100000, 0);
+        }
+    }
+}
