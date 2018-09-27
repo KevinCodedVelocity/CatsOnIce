6aecc13585fc22bc4e8a64cb46e813b6c4e72e53
Kevin <kevinh@codedvelocity.com>
2018-09-27 13:06:50 -0500

    Game state implementation

--- Assets/Code/GameController.cs
+++ Assets/Code/GameController.cs
@@ -1,22 +1,88 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
-    public GameObject gameWinVisuals;
+
+    public enum GameState
+    {
+        WaitingToStart,
+        Playing,
+        Dead,
+        Won
+    }
+
+    public GameEndVisualsController gameEndVisuals;
+    public GameObject titleVisuals;
+    public Button startButton;
     public Text titleText;
+    public CatController cat;
+
+    GameState gameState = GameState.WaitingToStart;
     const int TitleTextFadeDelay = 3;
     const float FadeSpeed = 5.0f;
 
+
 	// Use this for initialization
 	void Start ()
-    {	
-	}
+    {
+        startButton.onClick.AddListener(OnStartButtonClicked);
+
+    }
 
     // Update is called once per frame
     void Update()
     {
+        switch(gameState)
+        {
+            case GameState.WaitingToStart:
+                UpdateWaitingToStartState();
+                break;
+            case GameState.Playing:
+                UpdatePlayingState();
+                break;
+            case GameState.Dead:
+                UpdateDeadState();
+                break;
+            case GameState.Won:
+                UpdateWonState();
+                break;
+        }
+
+        
+    }
+
+    #region State machine methods
+
+    private void UpdateWonState()
+    {
+        gameEndVisuals.SetVisibility(true);
+        gameEndVisuals.SetText("You Won!");
+        SetTitleVisualsVisibility(false);
+    }
+
+    private void UpdateDeadState()
+    {
+        gameEndVisuals.SetVisibility(true);
+        gameEndVisuals.SetText("Game Over!");
+        SetTitleVisualsVisibility(false);
+    }
+
+    private void UpdatePlayingState()
+    {
+        gameEndVisuals.SetVisibility(false);
+        SetTitleVisualsVisibility(false);
+    }
+
+    private void UpdateWaitingToStartState()
+    {
+        cat.enabled = false;
+        gameEndVisuals.SetVisibility(false);
+        SetTitleVisualsVisibility(true);
+        titleText.text = "Cats On Ice!";
+
         if (Time.time > TitleTextFadeDelay)
         {
             if (titleText.color != Color.clear)
@@ -28,9 +94,36 @@ public class GameController : MonoBehaviour {
             }
         }
     }
+    
+    #endregion State machine methods
 
     public void OnFinishLineReached()
     {
-        gameWinVisuals.SetActive(true);
+        gameState = GameState.Won;
+        cat.enabled = false;
+    }
+
+    public void OnStartButtonClicked()
+    {
+        cat.enabled = true;
+        gameState = GameState.Playing;
+    }
+
+    public void RestartGame()
+    {
+        cat.enabled = false;
+        cat.Reset();
+        gameState = GameState.WaitingToStart;
+    }
+
+    private void SetTitleVisualsVisibility(bool isVisible)
+    {
+        titleVisuals.SetActive(isVisible);
+    }
+
+    public void OnDeath()
+    {
+        gameState = GameState.Dead;
+        cat.enabled = false;
     }
 }
---
+++ Assets/Code/GameEndVisualsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameEndVisualsController : MonoBehaviour {
+
+    public GameController gameController;
+
+    [SerializeField]
+    private Text gameEndText;
+    [SerializeField]
+    private Button tryAgainButton;
+
+    public void Start()
+    {
+        tryAgainButton.onClick.AddListener(gameController.RestartGame);
+    }
+
+    public void SetText(string text)
+    {
+        gameEndText.text = text;
+    }
+
+    public void SetVisibility(bool isVisible)
+    {
+        gameEndText.gameObject.SetActive(isVisible);
+        tryAgainButton.gameObject.SetActive(isVisible);
+    }
+}
